788f1747d5d68a98fb71cbe5bdc19f30d9240dc7
SamuelRamirezM <sramirezmu@unal.edu.co>
2024-08-28 08:38:01 -0500

    Enumerado CategoriaHabitacion

---
+++ gestorAplicacion/administracionHospital/CategoriaHabitacion.py
@@ -0,0 +1,10 @@
+#Importar lo necesario...
+from enum import Enum
+
+class CategoriaHabitacion(Enum):
+    CAMILLA = 50000
+    INDIVIDUAL = 150000
+    DOBLE = 320000
+    OBSERVACION = 500000
+    UCI = 1300000
+    UCC = 1500000
