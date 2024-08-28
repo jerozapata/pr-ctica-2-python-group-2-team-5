438be8fe41cffb6d1c425492ac9267cdd48a6f88
jerozapata <159493286+jerozapata@users.noreply.github.com>
2024-08-28 09:18:55 -0500

    Update pago

---
+++ gestorAplicacion/administracionHospital/Pago.py
@@ -0,0 +1,23 @@
+import Cita
+import CitaVacuna
+import Habitacion
+import Formula
+
+class Pago():
+
+    #constante Iva
+    IVA = 0.21
+
+    #metodos calcular precio
+    def calcular_precio(self, Formula):
+        pass
+    def calcular_precio(self, Cita):
+        pass
+    def calcular_precio(self, Cita_Vacuna):
+        pass
+    def calcular_precio(self, Habitacion):
+        pass
+
+
+
+
